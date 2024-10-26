98a43fac8e6a04ad505da40e8500163b73053f09
Charley Smith-Schofield <csmithschofield@gmail.com>
2024-10-25 17:41:16 -0700

    Adding search bar to filter pets by name of pet or by breed

--- pet_finder/src/components/PetList.tsx
+++ pet_finder/src/components/PetList.tsx
@@ -2,20 +2,39 @@ import { useState } from "react";
 import { Pet } from "../models/Pet";
 import { useEffect } from "react";
 import { getPets } from "../services/petService";
-import { Col, Row } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import { PetCard } from "./PetCard";
 
 export function PetList() {
   const [pets, setPets] = useState<Pet[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     getPets().then((pets) => setPets(pets));
   }, []);
 
+  const filteredPets = pets.filter(
+    (pet) => 
+    pet.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    pet.breed.toLowerCase().includes(searchQuery.toLowerCase()) 
+  );
+
   return(
     <div className="PetList">
+      <Form.Group controlId="search">
+        <Form.Control 
+        type="text"
+        placeholder="Search by pet name or breed"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </Form.Group>
       <Row>
-        {pets.map((pet) => (<Col lg={4}><PetCard pet={pet}/></Col>))}
+        {filteredPets.map((pets) => (
+          <Col lg={4} key={pets.id}>
+            <PetCard pet={pets} />
+          </Col>
+        ))}
       </Row>
     </div>
   )
